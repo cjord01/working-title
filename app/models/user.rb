9d8ddcb242ace0e02b4ccc57fdb27f1067902d7f
Theo Skolnik <theodoraskolnik@gmail.com>
2014-09-19 09:46:27 -0500

    deleted the good stuff

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ActiveRecord::Base
-  has_many :projects, foreign_key: "initiator_id"
-  has_many :versions, foreign_key: "contributor_id"
-  has_many :votes, foreign_key: "user_id"
-
-  has_secure_password
-end
